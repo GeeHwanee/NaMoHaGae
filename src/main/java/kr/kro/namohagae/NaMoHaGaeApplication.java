753cf5d628786a6b05d5e181a6242e7ead63316f
JiHwan Park <jihwanpark15@gmail.com>
2023-04-19 15:04:47 +0900

    Initial commit

---
+++ src/main/java/kr/kro/namohagae/NaMoHaGaeApplication.java
@@ -0,0 +1,13 @@
+package kr.kro.namohagae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaMoHaGaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaMoHaGaeApplication.class, args);
+	}
+
+}
