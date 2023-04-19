aa3a09d1ad52ea1c598e91d9f4c15a9283022dcb
Hnson <hnson321@gmail.com>
2023-04-19 16:31:42 +0900

    1st commit

--- src/main/java/kr/kro/namohagae/puchingtest/controller/PuchingMainController.java
+++ src/main/java/kr/kro/namohagae/puchingtest/controller/PuchingMainController.java
@@ -1,18 +1,14 @@
 package kr.kro.namohagae.puchingtest.controller;
 
-import kr.kro.namohagae.puchingtest.service.PuchingService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PuchingMainController {
-    @Autowired
-    private PuchingService service;
 
     @GetMapping("/puching/puching_main")
     public void puchingmain() {
-        service.town();
+
     };
 
 }
