736ecd4a713605985e8c23ac45c464ac19b1cb00
JiHwan Park <jihwanpark15@gmail.com>
2023-04-21 13:22:53 +0900

    create find.html update alarm_list.html update GlobalController.java update header.html update login.html update main.html
    by jihwan

--- src/main/java/kr/kro/namohagae/global/controller/GlobalController.java
+++ src/main/java/kr/kro/namohagae/global/controller/GlobalController.java
@@ -3,10 +3,15 @@ package kr.kro.namohagae.global.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/*
+    작성자: 박지환
+
+*/
+
 @Controller
 public class GlobalController {
 
-    // main 페이지
+    // [Global 파트]--------------------------------------------------------------------
     @GetMapping(value = {"/", "/main"})
     public String main(){
         return "main.html";
@@ -30,15 +35,34 @@ public class GlobalController {
     @GetMapping("/member/main")
     public String memberMain(){ return "/member/main.html";}
 
+    @GetMapping("/login")
+    public void login(){}
 
-    @GetMapping("/mall/cart")
-    public void cart(){
+    // [회원 파트]--------------------------------------------------------------------
+    @GetMapping("/member/join")
+    public void join(){}
 
-    }
+    @GetMapping("/member/find")
+    public void find(){}
+
+
+    // [퍼칭 파트]--------------------------------------------------------------------
     @GetMapping("/puching/chatroom")
     public void chatroom() {
-    };
+    }
+
+    // [게시판 파트]--------------------------------------------------------------------
 
 
+    // [쇼핑몰 파트]--------------------------------------------------------------------
+    @GetMapping("/mall/cart")
+    public void cart(){
+    }
+
+    // [관리자 파트]--------------------------------------------------------------------
+
+
+
+    // -------------------------------------------------------------------------------
 
 }
