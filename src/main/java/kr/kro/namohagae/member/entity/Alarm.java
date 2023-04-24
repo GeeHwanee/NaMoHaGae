132d17f6440d8c70ad2719603996a69eb5876c07
polopipi <dlswh6289@gmail.com>
2023-04-24 16:48:24 +0900

    @Alias 추가했습니다

--- src/main/java/kr/kro/namohagae/member/entity/Alarm.java
+++ src/main/java/kr/kro/namohagae/member/entity/Alarm.java
@@ -3,10 +3,12 @@ package kr.kro.namohagae.member.entity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import org.apache.ibatis.type.Alias;
 
 @Getter
 @AllArgsConstructor
 @Builder
+@Alias("Alarm")
 public class Alarm {
     private Integer alarmNo;
     private Integer memberNo;
