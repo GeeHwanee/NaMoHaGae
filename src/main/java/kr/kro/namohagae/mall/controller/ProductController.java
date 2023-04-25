6eba4a3672609dbe88408e925a86ddaeb92b9287
chaerry <racad@naver.com>
2023-04-25 14:59:59 +0900

    list, read 생성

--- src/main/java/kr/kro/namohagae/mall/controller/ProductController.java
+++ src/main/java/kr/kro/namohagae/mall/controller/ProductController.java
@@ -1,7 +1,31 @@
 package kr.kro.namohagae.mall.controller;
 
+import jakarta.servlet.http.HttpSession;
+import kr.kro.namohagae.mall.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class ProductController {
+    private final ProductService service;
+
+    @GetMapping("/mall/product/list")
+    public String list(Integer pageNo, Integer categoryNo, Integer memberNo, Model model, HttpSession session) {
+        System.out.println(categoryNo +"불러옴");
+        System.out.println("출력성공");
+        if(session.getAttribute("msg")!=null) {
+            model.addAttribute("msg", session.getAttribute("msg"));
+            session.removeAttribute("msg");
+        }
+        model.addAttribute("list", service.list(pageNo, categoryNo, memberNo));
+        return "/mall/product/list";
+    }
+
+    @GetMapping("/mall/product/read")
+    public void read(Integer productNo, Model model) {
+        model.addAttribute("product", service.read(productNo));
+    }
 }
