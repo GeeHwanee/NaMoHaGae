4981f01e8108b9a9bea1e5ef9409d2e584d968a7
JiHwan Park <jihwanpark15@gmail.com>
2023-04-26 10:32:49 +0900

    update Constants.java by jihwan

---
+++ src/main/java/kr/kro/namohagae/global/util/ImageConstants.java
@@ -0,0 +1,20 @@
+package kr.kro.namohagae.global.util;
+
+public interface ImageConstants {
+    public final static String IMAGE_BOARD_FOLDER = "./src/main/resources/static/image/board";
+    public final static String IMAGE_BOARD_PATH="/image/board";
+    public final static String IMAGE_BOARD_URL= "http://localhost:8081//image/board";
+    public final static String IMAGE_EMBEDED_FOLDER = "./src/main/resources/static/image/embeded";
+    public final static String IMAGE_EMBEDED_PATH="/image/embeded";
+    public final static String IMAGE_EMBEDED_URL="http://localhost:8081/image/embeded";
+    public final static String IMAGE_PRODUCT_FOLDER = "./src/main/resources/static/image/product";
+    public final static String IMAGE_PRODUCT_PATH="/image/product";
+    public final static String IMAGE_PRODUCT_URL="http://localhost:8081/image/product";
+    public final static String IMAGE_PROFILE_FOLDER = "./src/main/resources/static/image/profile";
+    public final static String IMAGE_PROFILE_PATH="/image/profile";
+    public final static String IMAGE_PROFILE_URL= "http://localhost:8081/image/profile";
+    public final static String IMAGE_TEMP_FOLDER ="./src/main/resources/static/image/temp";
+    public final static String IMAGE_TEMP_PATH="/image/temp";
+    public final static String IMAGE_TEMP_URL ="http://localhost:8081/image/temp";
+
+}
