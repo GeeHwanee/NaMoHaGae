04d12c555f9c2232e5dc5659384abfc9d6dec8eb
polopipi <dlswh6289@gmail.com>
2023-05-01 13:54:17 +0900

    리뷰 만들었습니다

---
+++ src/main/java/kr/kro/namohagae/puchingtest/entity/Review.java
@@ -0,0 +1,20 @@
+package kr.kro.namohagae.puchingtest.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class Review {
+    private Integer puchingReviewNo;
+    private Integer puchingNo;
+    private Integer puchingReviewWriter;
+    private Integer puchingReviewReceiver;
+    private String puchingReviewContent;
+    private Integer puchingReviewStar;
+    private LocalDateTime puchingReviewWriteDate;
+}
