cbe85fe29ee1b2072ed1f1c0e1d7711dfaa77248
chaerry <racad@naver.com>
2023-05-03 08:37:23 +0900

    count, findAllByProductNo 추가

--- src/main/java/kr/kro/namohagae/mall/dao/QnaDao.java
+++ src/main/java/kr/kro/namohagae/mall/dao/QnaDao.java
@@ -10,6 +10,12 @@ public interface QnaDao {
     // 큐엔에이 저장
     public Integer save(Qna qna);
 
+    // 특정 상품의 큐엔에이 개수를 조회
+    public Integer count(Integer productNo);
+
     // 상품 큐엔에이 보기
     public List<Qna> findByProductNo(Integer productNo);
+
+    // 특정 상품의 큐엔에이 목록 조회
+    public List<Qna> findAllByProductNo(Integer startRowNum, Integer endRowNum, Integer productNo);
 }
