0188206c8944fef487a190ec8db0f4eea566e860
chaerry <racad@naver.com>
2023-05-02 16:45:03 +0900

    pagination 추가

--- src/main/java/kr/kro/namohagae/mall/service/ProductReviewService.java
+++ src/main/java/kr/kro/namohagae/mall/service/ProductReviewService.java
@@ -19,6 +19,9 @@ public class ProductReviewService {
     private final ProductOrderDetailDao productOrderDetailDao;
     private final ProductDao productDao;
 
+    private final Integer PAGESIZE = 10;
+    private final Integer BLOCKSIZE = 5;
+
     @Transactional
     public void write(ProductReviewDto.Write dto, String memberEmail) {
         ProductReview productReview = dto.toEntity(memberEmail);
@@ -27,12 +30,28 @@ public class ProductReviewService {
         productDao.updateReview(productReview);
     }
 
-
     public ProductOrderDetail read(Integer orderDetailNo) {
         return productOrderDetailDao.findByOrderDetailNo(orderDetailNo);
     }
 
     public List<ProductReviewDto.Write> findInformationByProductNo(Integer productNo) {
         return productReviewDao.findInformationByProductNo(productNo);
+    }
+
+    public ProductReviewDto.Pagination list(Integer pageNo, Integer productNo) {
+        Integer startRowNum = (pageNo-1)*PAGESIZE + 1;
+        Integer endRowNum = startRowNum + PAGESIZE - 1;
+        List<ProductReview> reviews =  productReviewDao.findAllByProductNo(startRowNum, endRowNum, productNo);
+        Integer countOfReview = productReviewDao.count(productNo);
+        Integer countOfPage = (countOfReview-1)/PAGESIZE + 1;
+        Integer prev = (pageNo-1)/BLOCKSIZE * BLOCKSIZE;
+        Integer start = prev+1;
+        Integer end = prev + BLOCKSIZE;
+        Integer next = end+1;
+        if(end>=countOfPage) {
+            end = countOfPage;
+            next = 0;
+        }
+        return new ProductReviewDto.Pagination(pageNo, prev, start, end, next, productNo, reviews);
     }
 }
