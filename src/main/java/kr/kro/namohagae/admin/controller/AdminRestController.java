9e2f1605558ed03c0af90a3bfbbfd20cf0746113
JiHwan Park <jihwanpark15@gmail.com>
2023-05-03 16:50:39 +0900

    update AdminRestController.java by jihwan

--- src/main/java/kr/kro/namohagae/admin/controller/AdminRestController.java
+++ src/main/java/kr/kro/namohagae/admin/controller/AdminRestController.java
@@ -1,5 +1,10 @@
 package kr.kro.namohagae.admin.controller;
 
+import kr.kro.namohagae.mall.dto.ProductDto;
+import kr.kro.namohagae.mall.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class AdminRestController {
 
+    @Autowired
+    private ProductService productService;
 
+    @PostMapping("/admin/product/put")
+    public ResponseEntity<ProductDto.Read> productPut(ProductDto.Put dto) {
+        Integer result = productService.put(dto);
+
+
+        return ResponseEntity.ok(null);
+    }
 }
