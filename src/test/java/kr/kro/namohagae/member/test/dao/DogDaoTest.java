6414f84bbcbf41170686369bf80394a309a72f60
polopipi <dlswh6289@gmail.com>
2023-05-03 16:48:42 +0900

    테스트 한거 삭제했습니다

--- src/test/java/kr/kro/namohagae/member/test/dao/DogDaoTest.java
+++ src/test/java/kr/kro/namohagae/member/test/dao/DogDaoTest.java
@@ -12,12 +12,6 @@ public class DogDaoTest {
     @Autowired
     private DogDao dao;
 
-//    @Test
-    public void test1(){
-        Dog dog = Dog.builder().dogNo(1).memberNo(1).dogProfile("default.jpg").dogCategory("골든 리트리버").dogGender(false).dogIntroduce("우리개는 안물어요").dogName("릴딘").
-                dogBirthdayDate(LocalDateTime.now()).dogNotGenderEnabled(false).dogWeight(30.6).build();
-        dao.register(dog);
-    }
 
 
 }
