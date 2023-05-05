e2279064a8ad0d0ad1e7673cf8abf43316d213ef
JiHwan Park <jihwanpark15@gmail.com>
2023-05-05 16:57:23 +0900

    create MemberRestController.java
    by jihwan

--- src/main/java/kr/kro/namohagae/member/controller/MemberRestController.java
+++ src/main/java/kr/kro/namohagae/member/controller/MemberRestController.java
@@ -1,22 +1,14 @@
 package kr.kro.namohagae.member.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import kr.kro.namohagae.global.security.MyUserDetails;
-import kr.kro.namohagae.member.dto.MemberDto;
 import kr.kro.namohagae.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MemberRestController {
@@ -29,10 +21,10 @@ public class MemberRestController {
         // email은 중복 여부를 확인해야 한다 -> 중복되지 않는 경우 업데이트
         // 사진 + 이메일 -> 이메일이 겹치면 실패 -> 409를 보낸다
 			// 기존 내 이메일과 일치한다면 false 리턴
-		if (memberService.checkNickanme(memberNo,nickname)==false) {
+		if (!memberService.checkNickanme(memberNo, nickname)) {
 			return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
 		}
-        Boolean result = memberService.update(profile,nickname,introduce,memberNo,password,phone,townNo);
+        Boolean result = memberService.update(profile,nickname,memberNo,password,phone,townNo);
         return result? ResponseEntity.ok(null):ResponseEntity.status(HttpStatus.CONFLICT).body(null);
     }
 
