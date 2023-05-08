a0beacccf8c64701562c835647cea7e801eb51ba
polopipi <dlswh6289@gmail.com>
2023-05-08 11:51:30 +0900

    주소 검증식 만들었습니다

---
+++ src/main/java/kr/kro/namohagae/global/util/validation/Town.java
@@ -0,0 +1,20 @@
+package kr.kro.namohagae.global.util.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD,ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = TownValidator.class)
+public @interface Town {
+
+    String message()default "서비스하지 않는 지역입니다";
+
+    Class<?>[] groups() default {};
+    Class<? extends Payload> [] payload() default  {};
+}
