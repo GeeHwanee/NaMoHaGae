82e6b1a290fddea56259e6b09fe484996a2dc2c7
JiHwan Park <jihwanpark15@gmail.com>
2023-05-09 08:39:01 +0900

    update BoardNoticeService.java
    by jihwan

---
+++ src/main/java/kr/kro/namohagae/board/service/BoardNoticeService.java
@@ -0,0 +1,25 @@
+package kr.kro.namohagae.board.service;
+
+import kr.kro.namohagae.board.dao.BoardNoticeDao;
+import kr.kro.namohagae.board.dto.NoticeDto;
+import kr.kro.namohagae.board.entity.BoardNotice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BoardNoticeService {
+    @Autowired
+    private BoardNoticeDao boardNoticeDao;
+
+    public void addNotice(NoticeDto.Add dto){
+        BoardNotice boardNotice = dto.toEntity();
+        boardNoticeDao.save(boardNotice);
+    }
+
+
+    public List<BoardNotice> list() {
+        return boardNoticeDao.findAll();
+    }
+}
