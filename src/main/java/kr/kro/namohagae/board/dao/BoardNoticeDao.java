f915f1999159e38bc26fc627b2ea8a4cceb04a4a
JiHwan Park <jihwanpark15@gmail.com>
2023-05-09 09:12:26 +0900

    update boardNoticeMapper.xml
    by jihwan

--- src/main/java/kr/kro/namohagae/board/dao/BoardNoticeDao.java
+++ src/main/java/kr/kro/namohagae/board/dao/BoardNoticeDao.java
@@ -3,9 +3,13 @@ package kr.kro.namohagae.board.dao;
 import kr.kro.namohagae.board.entity.BoardNotice;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface BoardNoticeDao {
 
 
     public void save(BoardNotice boardNotice);
+
+    public List<BoardNotice> findAll();
 }
