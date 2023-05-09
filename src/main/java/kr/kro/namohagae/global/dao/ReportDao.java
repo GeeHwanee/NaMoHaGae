add001af02d714adf6f1be5900ea50ce764f810f
polopipi <dlswh6289@gmail.com>
2023-05-09 15:34:36 +0900

    reportDao 만들었습니다

---
+++ src/main/java/kr/kro/namohagae/global/dao/ReportDao.java
@@ -0,0 +1,20 @@
+package kr.kro.namohagae.global.dao;
+
+import kr.kro.namohagae.global.dto.ReportDto;
+import kr.kro.namohagae.global.entity.Report;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface ReportDao {
+    public Boolean save(Report report);
+
+    public Boolean delete(Integer reportNo);
+
+    public List<ReportDto.FindAll> findAll(Integer startRownum,Integer endRownum);
+
+    public List<ReportDto.FindAll> findAllByMemberNo(Integer memberNo,Integer startRownum,Integer endRownum);
+
+    public  Integer countReportByMemberNo(Integer memberNo);
+}
