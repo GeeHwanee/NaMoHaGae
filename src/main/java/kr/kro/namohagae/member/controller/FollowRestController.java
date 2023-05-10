516c3856f9f6eec7da3076a69faf75382ca58208
JiHwan Park <jihwanpark15@gmail.com>
2023-05-10 16:46:00 +0900

    update FollowRestController.java by jihwan

--- src/main/java/kr/kro/namohagae/member/controller/FollowRestController.java
+++ src/main/java/kr/kro/namohagae/member/controller/FollowRestController.java
@@ -4,14 +4,10 @@ import kr.kro.namohagae.global.security.MyUserDetails;
 import kr.kro.namohagae.global.websocket.WebSocketService;
 import kr.kro.namohagae.member.service.FollowService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
