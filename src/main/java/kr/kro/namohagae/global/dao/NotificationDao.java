49248e789dfa266990ffacc3d6f1a1f04a3b0203
JiHwan Park <jihwanpark15@gmail.com>
2023-05-10 16:43:39 +0900

    update NotificationDao.java by jihwan

--- src/main/java/kr/kro/namohagae/global/dao/NotificationDao.java
+++ src/main/java/kr/kro/namohagae/global/dao/NotificationDao.java
@@ -3,18 +3,22 @@ package kr.kro.namohagae.global.dao;
 import kr.kro.namohagae.global.dto.NotificationDto;
 import kr.kro.namohagae.global.entity.Notification;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface NotificationDao {
 
-    public void save(Notification notification);
+    public Integer save(Notification notification);
+
+    @Select("select NOTIFICATION_SEQ_NOTIFICATION_NO.currval from dual")
+    public Integer getSelectKey();
 
     public List<NotificationDto.FindAll> findAll(Integer startRownum, Integer endRownum, Integer memberNo);
     public Integer count(Integer memberNo);
 
-    public Integer notificationRead(Integer no);
+    public Integer update(Integer notificationNo);
 
-    public List<NotificationDto.FindAll> quikMenu(Integer memberNo);
+    public List<NotificationDto.FindAll> findByNotificationReadEnabled(Integer memberNo);
 }
