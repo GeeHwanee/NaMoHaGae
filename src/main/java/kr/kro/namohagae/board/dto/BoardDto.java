ae27bd8c5ca6b202a57fc66e3a9d13c9e7941147
polopipi <dlswh6289@gmail.com>
2023-05-10 16:41:15 +0900

    페이징이랑 내가쓴 글 만들었습니다

--- src/main/java/kr/kro/namohagae/board/dto/BoardDto.java
+++ src/main/java/kr/kro/namohagae/board/dto/BoardDto.java
@@ -1,10 +1,14 @@
 package kr.kro.namohagae.board.dto;
 
 import kr.kro.namohagae.board.entity.Board;
+import kr.kro.namohagae.member.dto.FollowDto;
 import kr.kro.namohagae.member.entity.Member;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class BoardDto {
     @Data
@@ -20,7 +24,26 @@ public class BoardDto {
         }
     }
 
+    @Data
+    public static class FindAllByMemberNo {
+        private Integer boardNo;
+        private Integer townNo;
+        private String boardTitle;
+        private Integer boardReadCount;
+        private Integer boardRecommendCount;
+        private Integer commentCount;
+        private LocalDateTime boardWriteDate;
+    }
 
-
-
+    @Data
+    @ToString
+    @AllArgsConstructor
+    public static class Pagination {
+        private Integer pageno;
+        private Integer prev;
+        private Integer start;
+        private Integer end;
+        private Integer next;
+        private List<BoardDto.FindAllByMemberNo> board;
+    }
 }
