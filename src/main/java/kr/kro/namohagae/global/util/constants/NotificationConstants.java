7b81c2995df3f938fa3df1777ac2ed326136d2c2
JiHwan Park <jihwanpark15@gmail.com>
2023-05-11 14:24:05 +0900

    update NotificationConstants.java.java
    by jihwan

--- src/main/java/kr/kro/namohagae/global/util/constants/NotificationConstants.java
+++ src/main/java/kr/kro/namohagae/global/util/constants/NotificationConstants.java
@@ -6,11 +6,13 @@ public interface NotificationConstants {
     public final static String COMMENT_CONTENT = "내 글에 새로운 댓글이 달렸습니다.";
     public final static String LIKE_CONTENT = "내 글에 새로운 추천이 생겼습니다.";
     public final static String CHAT_CONTENT = "새로운 메시지가 도착했습니다.";
+    public final static String QNA_CONTENT = "내 문의에 답변이 달렸습니다.";
     // Links
     public final static String PROFILE_LINK = "/member/profile?memberNo=";
     public final static String CHATROOM_LINK = "/puching/chatroom";
     public final static String BOARD_FREE_LINK = "/board/free/read?boardNo=";
     public final static String BOARD_TOWN_LINK = "/board/town/read?boardNo=";
+    public final static String QNA_LINK = "/member/main";
 
 
 
