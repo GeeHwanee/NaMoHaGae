82fb145963a3b660facceaa8c6da6722a224d8ee
chaerry <racad@naver.com>
2023-05-15 01:53:46 +0900

    updateStockByOrderFromProduct 생성

--- src/main/java/kr/kro/namohagae/mall/dao/ProductDao.java
+++ src/main/java/kr/kro/namohagae/mall/dao/ProductDao.java
@@ -24,9 +24,15 @@ public interface ProductDao {
     // 상품 상세 보기
     public ProductDto.Read findByProductNo(Integer productNo);
 
+    // 사용자가 구매하기 선택한 상품 검색
+    public ProductDto.Read findByMemberNoAndProductNo(Integer memberNo, Integer productNo);
+
     // 상품 번호 찾아서 재고 업데이트
     //public void updateStockByProductNo(List<Integer> productNos, Integer productOrderDetailNo);
     public void updateStockByProductNo(Map<String, Object> params);
+
+    // 상품 상세 보기에서 주문시 재고 업데이트
+    public void updateStockByOrderFromProduct(Map<String, Object> params);
 
     // 상품 번호로 상품 정보 불러오기
     public Integer findInformationByProductNo(Integer productNo);
