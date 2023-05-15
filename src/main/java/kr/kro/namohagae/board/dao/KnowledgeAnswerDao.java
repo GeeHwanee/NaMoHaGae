85f0a84c92be39c245a100288523db8c41f748c9
JiHwan Park <jihwanpark15@gmail.com>
2023-05-15 16:31:14 +0900

    update KnowledgeAnswerDao.java
    by jihwan

--- src/main/java/kr/kro/namohagae/board/dao/KnowledgeAnswerDao.java
+++ src/main/java/kr/kro/namohagae/board/dao/KnowledgeAnswerDao.java
@@ -1,7 +1,9 @@
 package kr.kro.namohagae.board.dao;
 
+import kr.kro.namohagae.board.entity.KnowledgeAnswer;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface KnowledgeAnswerDao {
+    public Integer save(KnowledgeAnswer knowledgeAnswer);
 }
