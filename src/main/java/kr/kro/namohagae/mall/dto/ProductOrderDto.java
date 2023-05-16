832af92bbff008d55484843021fd4e3b565c7be9
chaerry <racad@naver.com>
2023-05-16 16:41:24 +0900

    OrderResult 수정

--- src/main/java/kr/kro/namohagae/mall/dto/ProductOrderDto.java
+++ src/main/java/kr/kro/namohagae/mall/dto/ProductOrderDto.java
@@ -1,8 +1,5 @@
 package kr.kro.namohagae.mall.dto;
 
-import kr.kro.namohagae.mall.entity.Product;
-import kr.kro.namohagae.mall.entity.ProductImage;
-import kr.kro.namohagae.mall.entity.ProductOrderDetail;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -37,14 +34,9 @@ public class ProductOrderDto {
         private Integer productOrderTotalPrice;
         private LocalDateTime productOrderDate;
         private Integer memberNo;
-        //private Integer addressNo;
         private String addressPostcode;
         private String addressAddress;
         private String addressAddressDetail;
-        // private String productImage;
-        // 오더디테일디티오에서 상품명, 상품이미지,디테일리스트 끌고오기
-        private List<ProductImage> productImageList;
-        private List<Product> productList;
-        private List<ProductOrderDetail> orderDetailList;
+        private List<ProductOrderDetailDto.OrderDetailList> orderDetailList;
     }
 }
