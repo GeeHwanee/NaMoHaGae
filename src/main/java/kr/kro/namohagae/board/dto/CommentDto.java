8f37e237906da3b7fe853178015702a31c30d3c2
polopipi <dlswh6289@gmail.com>
2023-05-16 16:41:26 +0900

    내가 작성한 댓글 목록 dto 만들고 dto 수정했습니다

---
+++ src/main/java/kr/kro/namohagae/board/dto/CommentDto.java
@@ -0,0 +1,38 @@
+package kr.kro.namohagae.board.dto;
+
+import kr.kro.namohagae.mall.dto.QnaDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class CommentDto {
+    @Data
+    public static class CommentList{
+        private Integer memberNo;
+        private Integer boardNo;
+        private Integer commentNo;
+        private String memberNickname;
+        private String commentContent;
+        private LocalDateTime commentWriteDate;
+        private Boolean commentEnabled;
+
+    }
+    @Data
+    public static class MyCommentList{
+        private String qnaContent;
+        private String qnaAnswerContent;
+        private LocalDateTime qnaAnswerWriteDate;
+    }
+    @Data
+    @AllArgsConstructor
+    public static class PaginationMyCommentList {
+        private Integer pageNo;
+        private Integer prev;
+        private Integer start;
+        private Integer end;
+        private Integer next;
+        private List<CommentDto.MyCommentList> commentList;
+    }
+}
