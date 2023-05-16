15e859823085e4644ed5e506407e7e0df001a029
polopipi <dlswh6289@gmail.com>
2023-05-16 16:41:03 +0900

    내가 작성한 댓글 목록 서비스 만들었습니다

--- src/main/java/kr/kro/namohagae/board/controller/CommentController.java
+++ src/main/java/kr/kro/namohagae/board/controller/CommentController.java
@@ -4,11 +4,13 @@ package kr.kro.namohagae.board.controller;
 import kr.kro.namohagae.board.entity.BoardComment;
 import kr.kro.namohagae.board.service.CommentService;
 
+import kr.kro.namohagae.global.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -45,6 +47,10 @@ public class CommentController {
         System.out.println("댓글번호" + commentNo);
         commentService.commentDelete(commentNo);
         return ResponseEntity.ok(null);
+    }
+    public ResponseEntity<?> commentList(Integer pageno,@AuthenticationPrincipal MyUserDetails myUserDetails){
+        Integer memberNo= myUserDetails.getMemberNo();
+      return ResponseEntity.ok(commentService.myCommentList(pageno,memberNo));
     }
 
 }
