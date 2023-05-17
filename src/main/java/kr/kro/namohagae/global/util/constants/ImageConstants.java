4eb2d17aa9dc72ce92875936105854a2b6d3954c
JiHwan Park <jihwanpark15@gmail.com>
2023-05-17 23:30:31 +0900

    for test oracle instance
    by jihwan

--- src/main/java/kr/kro/namohagae/global/util/constants/ImageConstants.java
+++ src/main/java/kr/kro/namohagae/global/util/constants/ImageConstants.java
@@ -1,26 +1,26 @@
 package kr.kro.namohagae.global.util.constants;
 
 public interface ImageConstants {
-    public final String IMAGE_BOARD_FOLDER = "./src/main/resources/static/image/board";
+    public final String IMAGE_BOARD_FOLDER = "/home/jhp/image/board";
     public final String IMAGE_BOARD_PATH="/api/v1/image/board";
     public final String IMAGE_BOARD_URL= "http://localhost:8081/api/v1/image/board?name=";
-    public final String IMAGE_EMBEDED_FOLDER = "./src/main/resources/static/image/embeded";
+    public final String IMAGE_EMBEDED_FOLDER = "/home/jhp/image/embeded";
     public final String IMAGE_EMBEDED_PATH="/api/v1/image/embeded";
-    public final String IMAGE_EMBEDED_URL="http://localhost:8081/api/v1/image/embeded?name=";
-    public final String IMAGE_PRODUCT_FOLDER = "./src/main/resources/static/image/product";
+    public final String IMAGE_EMBEDED_URL="http://www.namohagae.kro.kr/api/v1/image/embeded?name=";
+    public final String IMAGE_PRODUCT_FOLDER = "/home/jhp/image/product";
     public final String IMAGE_PRODUCT_PATH="/api/v1/image/product";
     public final String IMAGE_PRODUCT_URL="http://localhost:8081/api/v1/image/product?name=";
-    public final String IMAGE_PROFILE_FOLDER = "./src/main/resources/static/image/profile";
+    public final String IMAGE_PROFILE_FOLDER = "/home/jhp/image/profile";
     public final String IMAGE_PROFILE_PATH="/api/v1/image/profile";
-    public final String IMAGE_PROFILE_URL= "http://localhost:8081/api/v1/image/profile?name=";
-    public final String IMAGE_TEMP_FOLDER ="./src/main/resources/static/image/temp";
+    public final String IMAGE_PROFILE_URL= "http://www.namohagae.kro.kr:8081/api/v1/image/profile?name=";
+    public final String IMAGE_TEMP_FOLDER ="/home/jhp/image/temp";
     public final String IMAGE_TEMP_PATH="/api/v1/image/temp";
     public final String IMAGE_TEMP_URL ="http://localhost:8081/api/v1/image/temp?name=";
-    public final String IMAGE_CHAT_FOLDER ="./src/main/resources/static/image/chat";
+    public final String IMAGE_CHAT_FOLDER ="/home/jhp/image/chat";
     public final String IMAGE_CHAT_PATH="/api/v1/image/chat";
-    public final String IMAGE_CHAT_URL ="http://localhost:8081/api/v1/image/chat?name=";
-    public final String IMAGE_DOG_FOLDER ="./src/main/resources/static/image/dog";
+    public final String IMAGE_CHAT_URL ="http://www.namohagae.kro.kr:8081/api/v1/image/chat?name=";
+    public final String IMAGE_DOG_FOLDER ="/home/jhp/image/dog";
     public final String IMAGE_DOG_PATH="/api/v1/image/dog";
-    public final String IMAGE_DOG_URL ="http://localhost:8081/api/v1/image/dog?name=";
+    public final String IMAGE_DOG_URL ="http://www.namohagae.kro.kr:8081/api/v1/image/dog?name=";
 
 }
