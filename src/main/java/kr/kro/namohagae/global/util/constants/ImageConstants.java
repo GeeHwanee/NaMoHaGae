96e860fe76d45e19d1ef8bcdf7fd01fe5777a44d
JiHwan Park <jihwanpark15@gmail.com>
2023-05-18 00:51:30 +0900

    for test server port
    by jihwan

--- src/main/java/kr/kro/namohagae/global/util/constants/ImageConstants.java
+++ src/main/java/kr/kro/namohagae/global/util/constants/ImageConstants.java
@@ -12,15 +12,15 @@ public interface ImageConstants {
     public final String IMAGE_PRODUCT_URL="http://localhost:8081/api/v1/image/product?name=";
     public final String IMAGE_PROFILE_FOLDER = "/home/jhp/image/profile";
     public final String IMAGE_PROFILE_PATH="/api/v1/image/profile";
-    public final String IMAGE_PROFILE_URL= "http://www.namohagae.kro.kr:8081/api/v1/image/profile?name=";
+    public final String IMAGE_PROFILE_URL= "http://www.namohagae.kro.kr/api/v1/image/profile?name=";
     public final String IMAGE_TEMP_FOLDER ="/home/jhp/image/temp";
     public final String IMAGE_TEMP_PATH="/api/v1/image/temp";
     public final String IMAGE_TEMP_URL ="http://localhost:8081/api/v1/image/temp?name=";
     public final String IMAGE_CHAT_FOLDER ="/home/jhp/image/chat";
     public final String IMAGE_CHAT_PATH="/api/v1/image/chat";
-    public final String IMAGE_CHAT_URL ="http://www.namohagae.kro.kr:8081/api/v1/image/chat?name=";
+    public final String IMAGE_CHAT_URL ="http://www.namohagae.kro.kr/api/v1/image/chat?name=";
     public final String IMAGE_DOG_FOLDER ="/home/jhp/image/dog";
     public final String IMAGE_DOG_PATH="/api/v1/image/dog";
-    public final String IMAGE_DOG_URL ="http://www.namohagae.kro.kr:8081/api/v1/image/dog?name=";
+    public final String IMAGE_DOG_URL ="http://www.namohagae.kro.kr/api/v1/image/dog?name=";
 
 }
