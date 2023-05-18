4851ebdbe66c0799f652e3b5f14fa56ba1f5b7e5
polopipi <dlswh6289@gmail.com>
2023-05-18 16:01:37 +0900

    오류 수정했습니다

--- src/main/java/kr/kro/namohagae/member/dto/MemberDto.java
+++ src/main/java/kr/kro/namohagae/member/dto/MemberDto.java
@@ -28,10 +28,10 @@ public class MemberDto {
         private Boolean memberCheckKaKao;
 
 
-        public Member toEntity(String encodedPassword, String memberProfileImage, String memberIntroduce,Integer townNo) {
+        public Member toEntity(String encodedPassword, String memberProfileImage, String memberIntroduce,Integer townNo,Boolean memberCheckKaKao) {
 
             return Member.builder().townNo(townNo).memberEmail(memberEmail).memberPassword(encodedPassword).memberNickname(memberNickname).memberPhone(memberPhone).
-                    memberIntroduce(memberIntroduce).memberProfileImage(memberProfileImage).memberLatitude(memberLatitude).memberLongitude(memberLongitude).memberCheckKaKao(false).build();
+                    memberIntroduce(memberIntroduce).memberProfileImage(memberProfileImage).memberLatitude(memberLatitude).memberLongitude(memberLongitude).memberCheckKaKao(memberCheckKaKao).build();
         }
 
     }
