3f7862dee362844a1cf2ea94682426900c5428a1
polopipi <dlswh6289@gmail.com>
2023-05-19 16:47:58 +0900

    블락 dto 만들었습니다

---
+++ src/main/java/kr/kro/namohagae/global/dto/BlockDto.java
@@ -0,0 +1,19 @@
+package kr.kro.namohagae.global.dto;
+
+import kr.kro.namohagae.global.entity.Block;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+public class BlockDto {
+    @Data
+    public static class save {
+        private Integer memberNo;
+        private Integer reportNo;
+        private String blockDeadlineDate;
+
+        public Block toEntity(LocalDateTime blockDeadlineDate){
+          return   Block.builder().memberNo(memberNo).reportNo(reportNo).blockDeadLineDate(blockDeadlineDate).build();
+        }
+    }
+}
