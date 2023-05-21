47b3c6974c09c2a181b0f80f8f78b0ced0415c75
JiHwan Park <jihwanpark15@gmail.com>
2023-05-21 22:29:06 +0900

    update WebSocketConfig.java
    fix AllowedOrigins
    
    by jihwan

--- src/main/java/kr/kro/namohagae/global/config/WebSocketConfig.java
+++ src/main/java/kr/kro/namohagae/global/config/WebSocketConfig.java
@@ -3,30 +3,29 @@ package kr.kro.namohagae.global.config;
 import kr.kro.namohagae.global.hander.NotificationWebSocketHandler;
 import kr.kro.namohagae.puching.websocket.ChatPuchingWebsocketHandler;
 import kr.kro.namohagae.puching.websocket.ChatWebSocketHandler;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
-    @Autowired
-    private ChatWebSocketHandler chatWebSocketHandler;
-    @Autowired
-    private ChatPuchingWebsocketHandler chatImageWebsocketHandler;
-    @Autowired
-    private NotificationWebSocketHandler notificationWebSocketHandler;
+
+    private final ChatWebSocketHandler chatWebSocketHandler;
+    private final ChatPuchingWebsocketHandler chatImageWebsocketHandler;
+    private final NotificationWebSocketHandler notificationWebSocketHandler;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         // 소켓연결 주소와 소켓접근가능 도메인 설정
         String os =  System.getProperty("os.name").toLowerCase();
         if (os.contains("linux")) {
-            registry.addHandler(chatWebSocketHandler,"/chatroom").setAllowedOrigins("https://namohagae.kro.kr");
-            registry.addHandler(chatImageWebsocketHandler,"/puching").setAllowedOrigins(("https://namohagae.kro.kr"));
-            registry.addHandler(notificationWebSocketHandler, "/notification").setAllowedOrigins("https://namohagae.kro.kr");
+            registry.addHandler(chatWebSocketHandler,"/chatroom").setAllowedOrigins("https://namohagae.kro.kr","https://www.namohagae.kro.kr");
+            registry.addHandler(chatImageWebsocketHandler,"/puching").setAllowedOrigins("https://namohagae.kro.kr","https://www.namohagae.kro.kr");
+            registry.addHandler(notificationWebSocketHandler, "/notification").setAllowedOrigins("https://namohagae.kro.kr","https://namohagae.kro.kr");
         } else{
             registry.addHandler(chatWebSocketHandler,"/chatroom").setAllowedOrigins("http://localhost:8081");
             registry.addHandler(chatImageWebsocketHandler,"/puching").setAllowedOrigins(("http://localhost:8081"));
