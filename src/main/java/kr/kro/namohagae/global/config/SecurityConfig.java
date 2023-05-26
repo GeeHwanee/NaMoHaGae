b8016dbb4ff5114154c4f11b65cb0ce782418bea
JiHwan Park <jihwanpark15@gmail.com>
2023-05-26 15:23:12 +0900

    update SecurityConfig.java update login.html
    by jihwan

--- src/main/java/kr/kro/namohagae/global/config/SecurityConfig.java
+++ src/main/java/kr/kro/namohagae/global/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(request -> request
                 .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
-                .requestMatchers("/login","/puching","/chatroom","/api/v1/**","/member/join","/member/addJoin","/member/kakaoJoin","/member/find","/css/**","/script/**").permitAll()
+                .requestMatchers("/login","/puching","/chatroom","/api/v1/**", "/image/**","/member/join","/member/addJoin","/member/kakaoJoin","/member/find","/css/**","/script/**").permitAll()
                 .anyRequest().authenticated());
 
         http.formLogin().loginPage("/login")
