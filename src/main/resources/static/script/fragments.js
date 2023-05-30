b0efdfcaf38c9ef1f13324e88e456fc3963fda04
JiHwan Park <jihwanpark15@gmail.com>
2023-05-30 11:22:35 +0900

    update fragments.js
    by jihwan

--- src/main/resources/static/script/fragments.js
+++ src/main/resources/static/script/fragments.js
@@ -110,7 +110,6 @@ function printAside(){
         $side_bar.append('<li><a href="/member/information">내 정보 수정</a></li>');
         $side_bar.append('<li><a href="/member/puching/review">내가 작성한 리뷰</a></li>');
         $side_bar.append('<li><a href="/member/follow">팔로우 목록</a></li>');
-        $side_bar.append('<li><a href="/member/mall/order">결제 내역</a></li>');
         $side_bar.append('<li><a href="/member/mall/address">배송지</a></li>');
         $side_bar.append('<li><a href="/member/mall/favorite">찜 목록</a></li>');
         $side_bar.append('<li><a href="/member/mall/order">주문 내역</a></li>');
