50f2d0018fd6cf3b43f6cc52958fbe699a12ef75
polopipi <dlswh6289@gmail.com>
2023-06-01 13:31:48 +0900

    최근 퍼칭 날짜 추가했습니다

--- src/main/java/kr/kro/namohagae/member/dto/FollowDto.java
+++ src/main/java/kr/kro/namohagae/member/dto/FollowDto.java
@@ -1,13 +1,18 @@
 package kr.kro.namohagae.member.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.ToString;
+
+import java.time.LocalDateTime;
 
 public class FollowDto {
     @Data
     public static class FollowList {
         private Integer followMemberNo;
+        @JsonFormat(pattern = "yy년 MM월 dd일 hh:mm:ss")
+        private LocalDateTime puchingDate;
         private String memberNickname;
         private Double memberGrade;
     }
