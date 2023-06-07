4951982db8e10340602a72e837ade9395aa49044
JiHwan Park <jihwanpark15@gmail.com>
2023-06-07 10:33:09 +0900

    update BoardNoticeService.java update boardNoticeMapper.xml
    by jihwan

--- src/main/java/kr/kro/namohagae/board/service/BoardNoticeService.java
+++ src/main/java/kr/kro/namohagae/board/service/BoardNoticeService.java
@@ -3,44 +3,35 @@ package kr.kro.namohagae.board.service;
 import kr.kro.namohagae.board.dao.BoardNoticeDao;
 import kr.kro.namohagae.board.dto.NoticeDto;
 import kr.kro.namohagae.board.entity.BoardNotice;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Service
 public class BoardNoticeService {
-    @Autowired
-    private BoardNoticeDao boardNoticeDao;
+
+    private final BoardNoticeDao boardNoticeDao;
 
     public void addNotice(NoticeDto.Add dto){
         BoardNotice boardNotice = dto.toEntity();
         boardNoticeDao.save(boardNotice);
-
-
     }
     public List<BoardNotice> list() {
         return boardNoticeDao.findAll();
     }
 
-    public BoardNotice read(Integer boardNoticeNo) {
+    public BoardNotice findByBoardNoticeNo(Integer boardNoticeNo) {
 
-        return boardNoticeDao.read(boardNoticeNo);
+        return boardNoticeDao.findByBoardNoticeNo(boardNoticeNo);
     }
     public Integer delete(Integer boardNoticeNo){
-
         return boardNoticeDao.delete(boardNoticeNo);
     }
 
-    public Integer increaseReadCnt(Integer boardNoticeNo){
-
-        return boardNoticeDao.increaseReadCnt(boardNoticeNo);
-    }
-
     public void update(NoticeDto.Update noticeDto){
-
         BoardNotice boardNotice = noticeDto.toEntity();
-
         boardNoticeDao.update(boardNotice);
     }
 }
