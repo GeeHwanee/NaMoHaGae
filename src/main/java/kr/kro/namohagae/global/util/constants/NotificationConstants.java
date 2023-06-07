48c82798294dd97b77a40ce868ff31cc79cd0c38
JiHwan Park <jihwanpark15@gmail.com>
2023-06-07 10:34:00 +0900

    update NotificationConstants.java
    by jihwan

--- src/main/java/kr/kro/namohagae/global/util/constants/NotificationConstants.java
+++ src/main/java/kr/kro/namohagae/global/util/constants/NotificationConstants.java
@@ -5,6 +5,8 @@ public interface NotificationConstants {
     public final static String FOLLOW_CONTENT = "님이 팔로우 했습니다.";
     public final static String COMMENT_CONTENT = "내 글에 새로운 댓글이 달렸습니다.";
     public final static String LIKE_CONTENT = "내 글에 새로운 추천이 생겼습니다.";
+    public final static String KNOWLEDGE_CONTENT = "내 질문에 새로운 답변이 달렸습니다.";
+    public final static String KNOWLEDGE_SELECT_CONTENT = "내 답변이 채택되었습니다.";
     public final static String CHAT_CONTENT = "새로운 메시지가 도착했습니다.";
     public final static String QNA_CONTENT = "내 문의에 답변이 달렸습니다.";
     // Links
@@ -12,7 +14,8 @@ public interface NotificationConstants {
     public final static String CHATROOM_LINK = "/puching/chatroom";
     public final static String BOARD_FREE_LINK = "/board/free/read?boardNo=";
     public final static String BOARD_TOWN_LINK = "/board/town/read?boardNo=";
-    public final static String QNA_LINK = "/member/main";
+    public final static String BOARD_KNOWLEDGE_LINK = "/board/knowledge/read?boardNo=";
+    public final static String QNA_LINK = "/member/mall/qna";
 
 
 
