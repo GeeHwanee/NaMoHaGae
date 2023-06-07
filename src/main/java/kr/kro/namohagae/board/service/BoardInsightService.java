49917f17e159d24f357bb652eefa9d885e175190
JiHwan Park <jihwanpark15@gmail.com>
2023-06-07 17:05:39 +0900

    update BoardInsightService.java
    by jihwan

--- src/main/java/kr/kro/namohagae/board/service/BoardInsightService.java
+++ src/main/java/kr/kro/namohagae/board/service/BoardInsightService.java
@@ -1,6 +1,8 @@
 package kr.kro.namohagae.board.service;
 
 import kr.kro.namohagae.board.dao.BoardInsightDao;
+import kr.kro.namohagae.global.service.NotificationService;
+import kr.kro.namohagae.member.dao.MemberDao;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,17 +10,23 @@ import org.springframework.stereotype.Service;
 @Service
 public class BoardInsightService {
 
+    private final MemberDao memberDao;
     private final BoardInsightDao boardInsightDao;
+    private final NotificationService notificationService;
+
+    public Integer save(Integer boardNo, Integer memberNo){
+        return boardInsightDao.save(boardNo, memberNo);
+    }
 
     public Boolean existsByBoardNoAndMemberNo(Integer boardNo, Integer memberNo){
         return boardInsightDao.existsByBoardNoAndMemberNo(boardNo, memberNo);
     }
 
-    public Boolean findBoardLikeEnabled(Integer boardNo, Integer memberNo){
-        return boardInsightDao.findBoardLikeEnabledByBoardNoAndMemberNo(boardNo, memberNo);
+    public Boolean findBoardRecommendEnabled(Integer boardNo, Integer memberNo){
+        return boardInsightDao.findBoardRecommendEnabledByBoardNoAndMemberNo(boardNo, memberNo);
     }
 
-    public void updateBoardLikeEnabled(Integer boardNo, Integer memberNo){
-        boardInsightDao.updateBoardLikeEnabled(boardNo, memberNo);
+    public void updateBoardRecommendEnabled(Integer boardNo, Integer boardMemberNo, Integer memberNo){
+        boardInsightDao.updateBoardRecommendEnabled(boardNo, memberNo);
     }
 }
