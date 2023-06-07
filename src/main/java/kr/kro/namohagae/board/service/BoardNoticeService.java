7816fe5403f6727b1b9de57499e3258f9cccd6fc
JiHwan Park <jihwanpark15@gmail.com>
2023-06-07 11:50:25 +0900

    update BoardNoticeService.java
    by jihwan

--- src/main/java/kr/kro/namohagae/board/service/BoardNoticeService.java
+++ src/main/java/kr/kro/namohagae/board/service/BoardNoticeService.java
@@ -1,7 +1,7 @@
 package kr.kro.namohagae.board.service;
 
 import kr.kro.namohagae.board.dao.BoardNoticeDao;
-import kr.kro.namohagae.board.dto.NoticeDto;
+import kr.kro.namohagae.board.dto.BoardNoticeDto;
 import kr.kro.namohagae.board.entity.BoardNotice;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,12 +14,12 @@ public class BoardNoticeService {
 
     private final BoardNoticeDao boardNoticeDao;
 
-    public void addNotice(NoticeDto.Add dto){
+    public void addNotice(BoardNoticeDto.Add dto){
         BoardNotice boardNotice = dto.toEntity();
         boardNoticeDao.save(boardNotice);
     }
-    public List<BoardNotice> list() {
-        return boardNoticeDao.findAll();
+    public List<BoardNoticeDto.Preview> preview() {
+        return boardNoticeDao.preview();
     }
 
     public BoardNotice findByBoardNoticeNo(Integer boardNoticeNo) {
@@ -30,7 +30,7 @@ public class BoardNoticeService {
         return boardNoticeDao.delete(boardNoticeNo);
     }
 
-    public void update(NoticeDto.Update noticeDto){
+    public void update(BoardNoticeDto.Update noticeDto){
         BoardNotice boardNotice = noticeDto.toEntity();
         boardNoticeDao.update(boardNotice);
     }
