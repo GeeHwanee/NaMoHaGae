35e763279413fe0f23d55153b19216ec1ad9fcf8
JiHwan Park <jihwanpark15@gmail.com>
2023-06-08 20:49:39 +0900

    update NaMoHaGaeApplicationTests.java
    by jihwan

--- src/test/java/kr/kro/namohagae/NaMoHaGaeApplicationTests.java
+++ src/test/java/kr/kro/namohagae/NaMoHaGaeApplicationTests.java
@@ -1,7 +1,24 @@
 package kr.kro.namohagae;
 
+import kr.kro.namohagae.board.dao.BoardDao;
+import kr.kro.namohagae.board.dto.BoardDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class NaMoHaGaeApplicationTests {
+
+    @Autowired
+    private BoardDao boardDao;
+    @Test
+    public void previewTest() {
+        List<BoardDto.Preview> list = boardDao.preview(1, "", "boardReadCount", 1, 10);
+        list.forEach(item -> System.out.println(item.toString()));
+        Integer result = boardDao.countPreview(1, "");
+        System.out.println("개수"+result);
+
+    }
 }
