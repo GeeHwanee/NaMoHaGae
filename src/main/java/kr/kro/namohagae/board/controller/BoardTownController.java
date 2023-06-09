5563586133d9b1156fe8939953a93f493f30944b
JiHwan Park <jihwanpark15@gmail.com>
2023-06-09 16:13:55 +0900

    update BoardTownController.java
    by jihwan

--- src/main/java/kr/kro/namohagae/board/controller/BoardTownController.java
+++ src/main/java/kr/kro/namohagae/board/controller/BoardTownController.java
@@ -1,6 +1,5 @@
 package kr.kro.namohagae.board.controller;
 
-import kr.kro.namohagae.board.dto.BoardTownDto;
 import kr.kro.namohagae.board.service.BoardService;
 import kr.kro.namohagae.board.service.BoardTownService;
 import kr.kro.namohagae.board.service.CommentService;
@@ -8,11 +7,6 @@ import kr.kro.namohagae.global.service.TownService;
 import kr.kro.namohagae.member.dao.MemberDao;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,27 +16,5 @@ public class BoardTownController {
     private final MemberDao memberDao;
     private final TownService townService;
     private final CommentService commentService;
-    @GetMapping("/board/town/write")
-    public String boardTownWrite(Model model) {
-        model.addAttribute("town",boardTownService.townList());
-        return "board/town/write";
-    }
-    @PostMapping("/board/town/writepro")
-    public String boardTownWritePro(BoardTownDto.write boardTownDto, Principal principal){
-
-
-
-        boardTownService.boardTownInsertData(boardTownDto,principal.getName());
-
-        return "redirect:/board/town/list";
-    }
-
-    @GetMapping("/board/town/delete")
-    public String boardTownDelete(Integer boardNo) {
-        boardTownService.townDeleteData(boardNo);
-
-        return "redirect:/board/town/list";
-    }
-
 
 }
