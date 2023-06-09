0b3338ada74d9bec479e08723e42377302e4f3e3
JiHwan Park <jihwanpark15@gmail.com>
2023-06-09 13:25:05 +0900

    update TownDao.java
    by jihwan

--- src/main/java/kr/kro/namohagae/global/dao/TownDao.java
+++ src/main/java/kr/kro/namohagae/global/dao/TownDao.java
@@ -2,18 +2,15 @@ package kr.kro.namohagae.global.dao;
 
 import kr.kro.namohagae.global.dto.TownDto;
 import kr.kro.namohagae.global.entity.Town;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
-import java.util.Optional;
 
 @Mapper
 public interface TownDao {
 
     public Integer findNoByDong(String townDong);
+    public String findTownDongByTownNo(Integer townNo);
     public List<TownDto.Dong> findDong();
     public List<TownDto.Read> findTownDongByGu(String townGu);
     public TownDto.userGuAndDong findFuck(Integer memberNo);
