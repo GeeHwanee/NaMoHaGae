5620490f0c959ce1d04074bf493070a2ad6529b2
JiHwan Park <jihwanpark15@gmail.com>
2023-06-09 13:24:52 +0900

    update MyUserDetailsService.java
    by jihwan

--- src/main/java/kr/kro/namohagae/global/security/MyUserDetailsService.java
+++ src/main/java/kr/kro/namohagae/global/security/MyUserDetailsService.java
@@ -1,5 +1,6 @@
 package kr.kro.namohagae.global.security;
 
+import kr.kro.namohagae.global.dao.TownDao;
 import kr.kro.namohagae.global.util.constants.Roles;
 import kr.kro.namohagae.member.dao.DogDao;
 import kr.kro.namohagae.member.dao.MemberDao;
@@ -24,22 +25,24 @@ import java.util.NoSuchElementException;
 @Service
 public class MyUserDetailsService implements UserDetailsService {
     private final MemberDao memberDao;
-    private final PasswordEncoder passwordEncoder;
+    private final TownDao townDao;
     private final DogDao dogDao;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         if(username.equals("admin")) {
-            return new MyUserDetails(-1, "관리자",0, username, passwordEncoder.encode("1234"), true, List.of(new SimpleGrantedAuthority(Roles.ADMIN.getRole())));
+            return new MyUserDetails(-1, "관리자",0,"", username, passwordEncoder.encode("1234"), true, List.of(new SimpleGrantedAuthority(Roles.ADMIN.getRole())));
         }
         try {
             Member member = memberDao.findByUsername(username).get();
+            String townDong = townDao.findTownDongByTownNo(member.getTownNo());
             Collection<GrantedAuthority> authorities = new ArrayList<>();
             authorities.add(new SimpleGrantedAuthority(Roles.USER.getRole()));
             if (dogDao.checkDogCountByMemberNo(member.getMemberNo())!=0){
                 authorities.add(new SimpleGrantedAuthority(Roles.DOG.getRole()));
             }
-            return new MyUserDetails(member.getMemberNo(), member.getMemberNickname(),member.getTownNo(),username, member.getMemberPassword(), member.getMemberEnabled(), authorities);
+            return new MyUserDetails(member.getMemberNo(), member.getMemberNickname(),member.getTownNo(), townDong,username, member.getMemberPassword(), member.getMemberEnabled(), authorities);
 
         }catch (NoSuchElementException e) {
             throw new UsernameNotFoundException("사용자를 찾을 수 없습니다");
