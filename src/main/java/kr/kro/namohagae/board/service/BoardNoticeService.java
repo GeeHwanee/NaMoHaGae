7849eba7841a1b89255391d32e591bcf8775acda
JiHwan Park <jihwanpark15@gmail.com>
2023-06-10 15:29:55 +0900

    update BoardNoticeService.java
    by jihwan

--- src/main/java/kr/kro/namohagae/board/service/BoardNoticeService.java
+++ src/main/java/kr/kro/namohagae/board/service/BoardNoticeService.java
@@ -3,6 +3,7 @@ package kr.kro.namohagae.board.service;
 import kr.kro.namohagae.board.dao.BoardNoticeDao;
 import kr.kro.namohagae.board.dto.BoardNoticeDto;
 import kr.kro.namohagae.board.entity.BoardNotice;
+import kr.kro.namohagae.global.util.pagination.Pagination;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -24,32 +25,13 @@ public class BoardNoticeService {
         return boardNoticeDao.preview();
     }
     public BoardNoticeDto.Pagination list(Integer pageNo) {
-        Integer start = (pageNo-1)*PAGESIZE + 1;
-        Integer end = start + PAGESIZE - 1;
-        List<BoardNoticeDto.Preview> notice =  boardNoticeDao.noticeList(start);
-        Integer countOfFavorite = boardNoticeDao.count();
-        Integer countOfPage = (countOfFavorite-1)/PAGESIZE + 1;
-        Integer prev = ((pageNo-1)/BLOCKSIZE) * BLOCKSIZE;
-        Integer startPage = prev+1;
-        Integer endPage = prev + BLOCKSIZE;
-        Integer next = endPage+1;
-        if(endPage>=countOfPage) {
-
-            endPage = countOfPage;
-            next = 0;
-        }
-        System.out.println(start);
-        System.out.println(end);
-        System.out.println(startPage);
-        System.out.println(endPage);
-//        System.out.println(notice.get(0).getBoardNoticeNo());
-//        System.out.println(notice.get(0).getBoardNoticeWriter());
-//        System.out.println(notice.get(0).getBoardNoticeWriteDate());
-        return new BoardNoticeDto.Pagination(pageNo, prev, startPage, endPage, next, notice);
+        Integer countOfNotice = boardNoticeDao.count();
+        Pagination page = new Pagination(BLOCKSIZE, PAGESIZE, pageNo, countOfNotice);
+        List<BoardNoticeDto.Preview> notice =  boardNoticeDao.noticeList(page.getStartRowNum());
+        return new BoardNoticeDto.Pagination(pageNo, page.getPrevPage(), page.getStartPage(), page.getEndPage(), page.getNextPage(), notice);
     }
 
     public BoardNoticeDto.Read findByBoardNoticeNo(Integer boardNoticeNo) {
-
         return boardNoticeDao.findByBoardNoticeNo(boardNoticeNo);
     }
     public Integer delete(Integer boardNoticeNo){
