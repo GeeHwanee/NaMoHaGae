e00bfdbabf98b6273e43648371a34250ed9dfc5d
JiHwan Park <jihwanpark15@gmail.com>
2023-06-10 15:06:12 +0900

    update Pagination.java
    by jihwan

--- src/main/java/kr/kro/namohagae/global/util/pagination/Pagination.java
+++ src/main/java/kr/kro/namohagae/global/util/pagination/Pagination.java
@@ -1,18 +1,23 @@
 package kr.kro.namohagae.global.util.pagination;
 
-public class Pagination {
-    public final Integer blockSize;
-    public final Integer pageSize;
-    public final Integer pageNo;
-    public final Integer countOfObject;
-    public final Integer startRowNum;
-    public final Integer endRowNum;
-    public final Integer countOfPage;
-    public final Integer prevPage;
-    public final Integer startPage;
-    public Integer endPage;
-    public Integer nextPage;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Pagination {
+    private final Integer blockSize;
+    private final Integer pageSize;
+    private final Integer pageNo;
+    private final Integer countOfObject;
+    private final Integer startRowNum;
+    private final Integer endRowNum;
+    private final Integer countOfPage;
+    private final Integer prevPage;
+    private final Integer startPage;
+    private Integer endPage;
+    private Integer nextPage;
 
     public Pagination(Integer blockSize, Integer pageSize, Integer pageNo, Integer countOfObject) {
         this.blockSize = blockSize;
@@ -20,19 +25,18 @@ public class Pagination {
         this.pageNo = pageNo;
         this.countOfObject = countOfObject;
         this.startRowNum = (this.pageNo-1)*pageSize + 1;
-        this.endRowNum = startRowNum+pageSize + 1;
+        this.endRowNum = startRowNum+pageSize - 1;
         this.countOfPage = (countOfObject-1)/pageSize + 1;
         this.prevPage =  ((pageNo-1)/blockSize) * blockSize;
         this.startPage = prevPage + 1;
         this.endPage = prevPage + blockSize;
         this.nextPage = endPage+1;
+        calculate();
+    }
+    private void calculate(){
         if(endPage>=countOfPage) {
             this.endPage = countOfPage;
             this.nextPage = 0;
         }
-
     }
-
-
-
 }
