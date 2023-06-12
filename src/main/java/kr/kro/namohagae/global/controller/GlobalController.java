60e2da2ccdcfebd4a89f223810128bdea9ecc97b
JiHwan Park <jihwanpark15@gmail.com>
2023-06-12 11:56:28 +0900

    update GlobalController.java update boardMapper.xml update post.html
    by jihwan

--- src/main/java/kr/kro/namohagae/global/controller/GlobalController.java
+++ src/main/java/kr/kro/namohagae/global/controller/GlobalController.java
@@ -1,43 +1,18 @@
 package kr.kro.namohagae.global.controller;
 
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
-import kr.kro.namohagae.board.dto.BoardTownDto;
-import kr.kro.namohagae.board.dto.KnowledgeQuestionDto;
-import kr.kro.namohagae.board.service.*;
+import kr.kro.namohagae.board.service.BoardService;
+import kr.kro.namohagae.board.service.BoardTownService;
 import kr.kro.namohagae.global.security.MyUserDetails;
-import kr.kro.namohagae.global.service.ReportService;
-import kr.kro.namohagae.global.service.TownService;
-import kr.kro.namohagae.mall.service.AddressService;
 import kr.kro.namohagae.mall.service.ProductService;
-import kr.kro.namohagae.mall.service.QnaService;
-import kr.kro.namohagae.member.dao.MemberDao;
-import kr.kro.namohagae.member.dto.DogDto;
-import kr.kro.namohagae.member.dto.MemberDto;
-import kr.kro.namohagae.member.service.DogService;
-import kr.kro.namohagae.member.service.MemberService;
-import kr.kro.namohagae.puching.dto.ReviewDto;
-import kr.kro.namohagae.puching.service.ChatService;
-import kr.kro.namohagae.puching.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.security.Principal;
 
 /*
     작성자: 박지환
@@ -85,10 +60,10 @@ public class GlobalController {
     public String boardMain(Model model){
 
 
-        model.addAttribute("ReadList",boardService.mainReadList());
-        model.addAttribute("RecommendList",boardService.mainRecommendList());
-        model.addAttribute("TownReadList",boardTownService.mainReadList());
-        model.addAttribute("TownRecommendList",boardTownService.mainRecommendList());
+       model.addAttribute("ReadList",boardService.mainPreview(0,"boardReadCount"));
+        model.addAttribute("RecommendList",boardService.mainPreview(0,"boardRecommendCount"));
+        model.addAttribute("TownReadList",boardService.mainPreview(null,"boardReadCount"));
+        model.addAttribute("TownRecommendList",boardService.mainPreview(null,"boardRecommendCount"));
         return "board/main";
     }
 
